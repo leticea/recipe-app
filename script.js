a710b77ecfd2872d3dea17805fd56ec0715b9789
leticea <nascimento.leticeah@gmail.com>
2023-04-22 09:05:49 -0300

    feat(script.js): js variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const resultContainer = document.getElementById("result");
+const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const seachContainer = document.querySelector("search-box");
+
