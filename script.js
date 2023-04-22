99b08cccc4bef46c30ac2e10df9ebfcd7944f281
leticea <nascimento.leticeah@gmail.com>
2023-04-22 09:10:34 -0300

    feat(script.js): event listener function

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const searchBtn = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const seachContainer = document.querySelector("search-box");
 
+// Api url to fetch meal data
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+// Event listeners for search and input (when press enter)
+searchBtn.addEventListener("click", searchMeal);
+searchInput.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    searchMeal();
+  }
+});
